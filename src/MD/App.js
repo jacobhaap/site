1e962d11bfca90a31b00255eab55e1708034fca0
Jacob Haap <git@j3.cx>
2023-10-24 19:03:38 +0200

    universal footer

--- src/MD/App.js
+++ src/MD/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MarkdownRender from './MarkdownRender';
+import Footer from './Footer';
 
 function App() {
     return (
         <div className="App">
             <MarkdownRender fileName="README.md" />
+            <Footer />
         </div>
     );
 }
---
+++ src/MD/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="universal-footer">
+            <p>Copyright ©2023 Jacob Haap. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
