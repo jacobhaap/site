ff5f08c54d9527b80103288ef66756e0fbbc80c0
Jacob Haap <git@j3.cx>
2023-11-03 00:52:33 +0100

    size support

--- src/MD/MarkdownRender.js
+++ src/MD/MarkdownRender.js
@@ -24,17 +24,36 @@ const components = {
     },
     img: ({ alt, src, title }) => {
         let className = '';
+        let width = '';
+        let height = '';
     
-        // Match the class pattern from the alt text
-        const classPattern = /\[(hard-corner|round-corner)\]$/;
-        const match = alt.match(classPattern);
+        // Match the class, width, and height pattern from the alt text
+        const pattern = /\[(hard-corner|round-corner)(?:,width=(\d+px))?(?:,height=(\d+px))?\]$/;
+        const match = alt.match(pattern);
     
         if (match) {
           className = match[1]; // The captured class name (hard-corner or round-corner)
-          alt = alt.replace(classPattern, '').trim(); // Remove the class pattern from the alt text
+    
+          // Check if width and height are present
+          if (match[2]) {
+            width = match[2];
+          }
+          if (match[3]) {
+            height = match[3];
+          }
+    
+          alt = alt.replace(pattern, '').trim(); // Remove the class pattern from the alt text
         }
     
-        return <img src={src} alt={alt} title={title} className={className} />;
+        return (
+          <img 
+            src={src} 
+            alt={alt} 
+            title={title} 
+            className={className}
+            style={{ width: width, height: height }}
+          />
+        );
       }
 };
 
