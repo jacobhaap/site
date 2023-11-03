89d87b942fdcdefd7b2a5f00e336be6c1ed03a44
Jacob Haap <git@j3.cx>
2023-11-03 01:02:27 +0100

    image alignment

--- src/MD/RenderComponents/IMG.js
+++ src/MD/RenderComponents/IMG.js
@@ -2,9 +2,10 @@ const Component_IMG = ({ alt, src, title }) => {
     let className = '';
     let width = '';
     let height = '';
-    
-    // Match the class, width, and height pattern from the alt text
-    const pattern = /\[(hard-corner|round-corner)(?:,width=(\d+px))?(?:,height=(\d+px))?\]$/;
+    let alignStyle = {};
+
+    // Match the class, width, height, and alignment pattern from the alt text
+    const pattern = /\[(hard-corner|round-corner)(?:,width=(\d+px))?(?:,height=(\d+px))?(?:,align=(left|center|right))?\]$/;
     const match = alt.match(pattern);
     
     if (match) {
@@ -17,6 +18,16 @@ const Component_IMG = ({ alt, src, title }) => {
       if (match[3]) {
         height = match[3];
       }
+
+      // Extract alignment and set styles
+      const alignment = match[4];
+      if (alignment === "left") {
+        alignStyle = { display: 'block', marginLeft: '0', marginRight: 'auto' };
+      } else if (alignment === "center") {
+        alignStyle = { display: 'block', marginLeft: 'auto', marginRight: 'auto' };
+      } else if (alignment === "right") {
+        alignStyle = { display: 'block', marginLeft: 'auto', marginRight: '0' };
+      }
     
       alt = alt.replace(pattern, '').trim(); // Remove the class pattern from the alt text
     }
@@ -27,7 +38,7 @@ const Component_IMG = ({ alt, src, title }) => {
         alt={alt} 
         title={title} 
         className={className}
-        style={{ width: width, height: height }}
+        style={{ width: width, height: height, ...alignStyle }} // Spread the alignStyle to apply the alignment
       />
     );
 }
